6f00cdb99db62cf02ed8b8b57e2f96b6700a6804
starwilly <willystar@gmail.com>
2018-05-26 15:36:48 +0800

    Setup firebase

---
+++ src/firebaseInit.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import 'firebase/firestore'
+
+const config = {
+    apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+    authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+    projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`
+};
+
+const firestoreSettings = { timestampsInSnapshots: true };
+
+firebase.initializeApp(config)
+
+const firestore = firebase.firestore()
+firestore.settings(firestoreSettings)
+
+
+export { firestore }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { firestore as db } from "./firebaseInit";
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+db.collection('posts').get().then(querySnapshot => {
+    querySnapshot.forEach(doc => {
+        console.log('doc', doc.data())
+    })
+})
